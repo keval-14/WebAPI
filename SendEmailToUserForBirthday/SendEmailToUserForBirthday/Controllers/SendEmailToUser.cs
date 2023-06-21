1f9952e954e8503191f1d84d85cc41742a0846bd
Keval Mangukiya <work.kevalpatel@gmail.com>
2023-06-21 18:22:11 +0530

    birthday api and service in one project

--- SendEmailToUserForBirthday/SendEmailToUserForBirthday/Controllers/SendEmailToUser.cs
+++ SendEmailToUserForBirthday/SendEmailToUserForBirthday/Controllers/SendEmailToUser.cs
@@ -15,6 +15,8 @@ namespace SendEmailToUserForBirthday.Controllers
     [ApiController]
     public class SendEmailToUser : ControllerBase
     {
+
+        #region Dependancy Injection
         private readonly SqlPracticeContext _sqlPracticeContext;
         private readonly IConfiguration _configuration;
 
@@ -25,80 +27,79 @@ namespace SendEmailToUserForBirthday.Controllers
             _sqlPracticeContext = sqlPracticeContext;
             _configuration = configuration;
             ConnectionString = configuration.GetConnectionString("practiceDB");
-            providerName = "using System.Data.SqlClient";
+            //providerName = "using System.Data.SqlClient";
         }
 
+        #endregion
+
+        #region SQL Connection
         public IDbConnection Connection
         {
             get { return new SqlConnection(ConnectionString); }
         }
 
+        #endregion
+
+
+
+        /// <summary>
+        /// Returns List of Employee whoes birthday is exactly one month later from today and send them Email for Birthday reminder
+        /// </summary>
+        /// <returns></returns>
+        
+        #region Get Employees
         [HttpGet]
         public IActionResult EmpList()
         {
-            List<Employee> employees = new List<Employee>();
-            using (IDbConnection dbConnection = Connection)
+            try
             {
-                dbConnection.Open();
-                DynamicParameters param = new DynamicParameters();
-
-                employees = dbConnection.Query<Employee>("SP_SendMailToUserForBday", commandType: CommandType.StoredProcedure).ToList();
-                dbConnection.Close();
-
-
-                if (employees.Count() >= 1)
+                List<Employee> employees = new List<Employee>();
+                using (IDbConnection dbConnection = Connection)
                 {
+                    dbConnection.Open();
+                    employees = dbConnection.Query<Employee>("SP_SendMailToUserForBday", commandType: CommandType.StoredProcedure).ToList();
+                    dbConnection.Close();
 
-                    foreach (var emp in employees)
+                    if (employees.Count() >= 1)
                     {
-
-                        //if ((bool)emp.IsMailSent && emp.MailSentDate <= DateTime.Now)
-                        //{
-
-                        //}
-                        //else
-                        //{
-
-                        //}
-
-                        var fromAddress = new MailAddress("evanzandu@gmail.com", $"Hey! {emp.FirstName}...!");
-                        var toAddress = new MailAddress(emp.Address);
-                        var subject = "Your Birthday Is Coming Soon...!";
-                        var body = $"Hi, Your Birthday Is Coming Soon on {emp.DateofBirth?.ToString("dd-MM-yyyy")}";
-                        var message = new MailMessage(fromAddress, toAddress)
+                        foreach (var emp in employees)
                         {
-                            Subject = subject,
-                            Body = body,
-                            IsBodyHtml = true
-                        };
-                        var smtpClient = new SmtpClient("smtp.gmail.com", 587)
-                        {
-                            UseDefaultCredentials = false,
-                            Credentials = new NetworkCredential("evanzandu@gmail.com", "timrrquqhqzvdpns"),
-                            EnableSsl = true
-                        };
-                        smtpClient.Send(message);
+                            var fromAddress = new MailAddress("evanzandu@gmail.com", $"Hey! {emp.FirstName}...!");
+                            var toAddress = new MailAddress(emp.Address);
+                            var subject = "Your Birthday Is Coming Soon...!";
+                            var body = $"Hi, Your Birthday Is Coming Soon on {emp.DateofBirth?.ToString("dd-MM-yyyy")}";
+                            var message = new MailMessage(fromAddress, toAddress)
+                            {
+                                Subject = subject,
+                                Body = body,
+                                IsBodyHtml = true
+                            };
+                            var smtpClient = new SmtpClient("smtp.gmail.com", 587)
+                            {
+                                UseDefaultCredentials = false,
+                                Credentials = new NetworkCredential("evanzandu@gmail.com", "timrrquqhqzvdpns"),
+                                EnableSsl = true
+                            };
+                            smtpClient.Send(message);
 
-                        emp.IsMailSent = true;
-                        emp.MailSentDate = DateTime.Now;
-
+                            emp.IsMailSent = true;
+                            emp.MailSentDate = DateTime.Now;
+                            _sqlPracticeContext.Employees.Update(emp);
+                            _sqlPracticeContext.SaveChanges();
+                        }
                     }
-                    _sqlPracticeContext.SaveChanges();
+                    return StatusCode(200, $"{employees.Count()} emails sent..!");
+                        //Ok($"{employees.Count()} emails sent..!");
                 }
-                return Ok(employees);
-
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error: " + ex.InnerException.Message);
             }
 
-
-
-
-
-
-
-
-
-
         }
+        #endregion
     }
 }
+
 
